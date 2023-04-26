e56e5c88d5ce33bbf0f9e59796f4672462919187
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-26 11:22:32 +0200

    Make operations work with 1 digit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// ELEMENTS
+let first;
+let second;
+let operator;
+let solution;
+
 // OPERATIONS
 function add (a, b) {
     return a + b;
@@ -25,25 +31,88 @@ function sqRoot (a) {
     return Math.sqrt(a);
 }
 
-// PARTS OF CALCULATION
-let first;
-let second;
-let operator;
+function percent(a) {
+    return a / 100;
+}
 
 // OPERATE FUNCTION
-function operate (a, b, oper) {
-    switch (oper) {
-        case '+':
-            add(a, b);
+function operate (x, y, operation) {
+    switch (operation) {
+        case 'add':
+            solution = add(x, y);
             break;
-        case '-':
-            subtract(a, b);
+        case 'subtr':
+            solution = subtract(x, y);
             break;
-        case '*':
-            multiply(a, b);
+        case 'mult':
+            solution = multiply(x, y);
             break;
-        case '/':
-            divide(a, b);
+        case 'div':
+            solution = divide(x, y);
             break;
     }
+    return solution;
 }
+
+// Numbers on display
+const dispNum = document.querySelector('#dispNum');
+
+function displayNumber(e) {
+    let num = e.target.textContent;
+    dispNum.textContent = num;
+}
+
+const numbers = document.querySelectorAll('.num');
+numbers.forEach(number => number.addEventListener('click', displayNumber));
+
+let counter = 0;
+
+numbers.forEach(number => number.addEventListener('click', (e) => {
+    if (counter == 0) {
+        first = e.target.textContent;
+    }
+    console.log(counter);
+    console.log(first);
+}));
+
+numbers.forEach(number => number.addEventListener('click', (e) => {
+    if (counter == 1) { 
+        second = e.target.textContent;
+    }
+    console.log(second);
+}));
+
+const opers = document.querySelectorAll('.oper');
+opers.forEach(oper => oper.addEventListener('click', (e) => {
+    counter++;
+    operator = e.target.id;
+    e.target.classList.toggle('operClicked');
+}))
+
+const equal = document.querySelector('#equal');
+equal.addEventListener('click', () => {
+    counter = 0;
+    operate(first, second, operator);
+    console.log(solution);
+    dispNum.textContent = solution;
+});
+
+const sqrt = document.querySelector('#sqrt');
+sqrt.addEventListener('click', (e) => {
+    e.target.classList.add('operClicked');
+    if (counter == 0) {
+        solution = sqRoot(first);
+        console.log(solution);
+        dispNum.textContent = solution;
+    }
+})
+
+const perc = document.querySelector('#perc');
+perc.addEventListener('click', (e) => {
+    e.target.classList.add('operClicked');
+    if (counter == 0) {
+        solution = percent(first);
+        console.log(solution);
+        dispNum.textContent = solution;
+    }
+})
